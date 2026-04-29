48d80a68b127dbfb900919e6ff5f8bf0f6fcb6a2
Griffin <griffinbkeeler@gmail.com>
2026-04-29 16:25:20 -0500

    Updated rsvp to include sequences of 20 trials each

--- src/rsvp/rsvp.py
+++ src/rsvp/rsvp.py
@@ -1,15 +1,17 @@
 import random
 
-from psychopy import visual, core
+from psychopy import visual, core, event
 
 # -------------
 # Experiment Settings
 # -------------
 
-target_letter = "E"
+target_letter = "X"
 non_target_letters = ["A", "B", "C", "E", "L", "M", "O", "R", "S"]
 
-n_trials = 100
+n_trials = 200
+n_sequences = 100
+sequence_length = 20
 target_probability = 0.20
 
 stimulus_duration = 0.1875
@@ -19,28 +21,27 @@ interstimulus_interval = 0.09375
 # Create trial list
 # -------------
 
-n_targets = int(n_trials * target_probability)
+n_targets = int(n_sequences * target_probability)
 n_non_targets = n_trials - n_targets
 
-trials = []
+sequences = []
 
-# Add targets to list
-for _ in range(n_targets):
-    trials.append({
-        "letter" : target_letter,
-        "condition" : "target"
-    })
+for sequence in range(n_sequences):
+    trials = []
 
-# Add non-targets to list
-for _ in range(n_non_targets):
-    letter = random.choice(non_target_letters)
-    trials.append({
-        "letter": letter,
-        "condition" : "non_target"
-    })
+    # Add non-targets to list
+    for _ in range(sequence_length):
+        letter = random.choice(non_target_letters)
+        trials.append(letter)
+        if sequence < 20:
+            trials[0] = target_letter
 
-# Randomize order of trials
-random.shuffle(trials)
+    # Randomize order of trials
+    random.shuffle(trials)
+    sequences.append({"sequence": sequence, "trials": trials})
+
+# Randomize order of sequences
+random.shuffle(sequences)
 
 # -------------
 # PsychoPy Setup
@@ -49,22 +50,12 @@ random.shuffle(trials)
 win = visual.Window(size=(800, 600), color="black", units="pix")
 
 # Text stimulus
-text_stimulus = visual.TextStim(
-    win=win,
-    text="",
-    color="white",
-    height=80,
-    pos=(0, 0)
-)
+text_stimulus = visual.TextStim(win=win, text="", color="white", height=80, pos=(0, 0))
 text_stimulus.text = target_letter
 
 # Fixation stimulus
 fixation_stimulus = visual.TextStim(
-    win=win,
-    text="+",
-    color="white",
-    height=80,
-    pos=(0, 0)
+    win=win, text="+", color="white", height=80, pos=(0, 0)
 )
 
 # Independent clock for tracking time
@@ -88,32 +79,37 @@ clock.reset()
 
 # Gap between target and RSVP sequence
 text_stimulus.text = ""
-while clock.getTime() < 0.300:
+while clock.getTime() < 0.750:
     text_stimulus.draw()
     win.flip()
 
-for trial in trials:
-    text_stimulus.text = trial["letter"]
+for i in range(n_sequences):
 
-    # Display letter
-    clock.reset()
-    while clock.getTime() < stimulus_duration:
+    trials = sequences[i]["trials"]
+    for trial in trials:
+        text_stimulus.text = trial
+
+        # Display letter
+        clock.reset()
+        while clock.getTime() < stimulus_duration:
+            text_stimulus.draw()
+            win.flip()
+
+        # Inter-stimulus interval
+        clock.reset()
+        while clock.getTime() < interstimulus_interval:
+            fixation_stimulus.draw()
+            win.flip()
+
+    # Gap between sequences
+    text_stimulus.text = ""
+    while clock.getTime() < 2:
         text_stimulus.draw()
         win.flip()
-
-    # Inter-stimulus interval
     clock.reset()
-    while clock.getTime() < interstimulus_interval:
-        fixation_stimulus.draw()
-        win.flip()
+
+    if event.getKeys(["escape"]):
+        break
 
 win.close()
 core.quit()
-
-
-
-
-
-
-
-
